7b7f082117dc035576bb63dce62e81a20e7a93f3
tibbsm <tibbs.marc@gmail.com>
2021-01-26 23:02:44 +0900

    Adding even odd solution

--- even_odd/main.go
+++ even_odd/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Even Odd")
+	a := make([]int, 11)
+	for i := range a {
+		if i%2 == 0 {
+			fmt.Printf("%d is even.\n", i)
+		} else {
+			fmt.Printf("%d is odd.\n", i)
+		}
+	}
 }
+
+// Pretty straight forward execution. I googled the best way (?) to do a for
+// in Go and used that. Then I was left with a modulo operator problem or
+// finding out if it was even or odd. Had some trouble with fmt.Println(), so I
+// Googled some more and found that printf() was better for this case. Println()
+// doesn't do formatting and Printf() does.
