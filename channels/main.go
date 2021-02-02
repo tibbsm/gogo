82f6d49837410591c4699f772d04cd08516c4369
tibbsm <tibbs.marc@gmail.com>
2021-02-02 22:41:53 +0900

    go rountines and channels

---
+++ channels/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.apple.com",
+		"https://www.amazon.com",
+		"https://www.stackoverflow.com",
+		"https://www.golang.org",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down.")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up.")
+	c <- link
+}
